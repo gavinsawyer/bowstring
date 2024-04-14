ec0240f71fd48fe226ec79414b7ecca3f569adaa
Gavin Sawyer <g@vinsawyer.dev>
2024-04-14 01:36:03 -0400

    v17.7.2
    - Improved FooterComponent sticky behavior further.

--- libs/components/src/lib/footer/FooterComponent.ts
+++ libs/components/src/lib/footer/FooterComponent.ts
@@ -1,20 +1,11 @@
-import { DOCUMENT, isPlatformBrowser }                                                                     from "@angular/common";
-import { Component, computed, ElementRef, inject, PLATFORM_ID, Signal, signal, ViewChild, WritableSignal } from "@angular/core";
-import { toObservable, toSignal }                                                                                             from "@angular/core/rxjs-interop";
-import { combineLatest, delayWhen, distinctUntilChanged, EMPTY, filter, fromEvent, map, merge, Observable, startWith, timer } from "rxjs";
+import { DOCUMENT, isPlatformBrowser }                                                                                                                    from "@angular/common";
+import { AfterViewInit, Component, ElementRef, inject, NgZone, PLATFORM_ID, Signal, signal, ViewChild, WritableSignal }                                   from "@angular/core";
+import { toObservable, toSignal }                                                                                                                         from "@angular/core/rxjs-interop";
+import { combineLatest, delayWhen, distinctUntilChanged, EMPTY, fromEvent, map, merge, Observable, Observer, startWith, switchMap, TeardownLogic, timer } from "rxjs";
 
 
 @Component({
   exportAs:    "standardFooter",
-  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
-  host:        {
-    "[class.lowering]":                  "(stuckOrUnsticking$() && !stuck$()) || (raisedOrLowering$() && !raised$())",
-    "[class.raised]":                    "stuckOrUnsticking$() && raisedOrLowering$()",
-    "[class.stuck]":                     "stuckOrUnsticking$()",
-    "[class.unsticking]":                "stuckOrUnsticking$() && !stuck$()",
-    "[style.--container-offset-bottom]": "containerOffsetBottom$() + 'px'",
-    "[style.--unsticking-translation]":  "unstickingTranslation$() + 'px'",
-  },
   selector:    "standard--footer",
   standalone:  true,
   styleUrls:   [
@@ -22,7 +13,13 @@ import { combineLatest, delayWhen, distinctUntilChanged, EMPTY, filter, fromEven
   ],
   templateUrl: "FooterComponent.html",
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+
+  @ViewChild("divHtmlElement", {
+    read:   ElementRef<HTMLDivElement>,
+    static: true,
+  })
+  private readonly divHtmlElementRef?:    ElementRef<HTMLDivElement>;
 
   @ViewChild("footerHtmlElement", {
     read:   ElementRef<HTMLElement>,
@@ -30,31 +27,34 @@ export class FooterComponent {
   })
   private readonly footerHtmlElementRef?: ElementRef<HTMLElement>;
 
-  private readonly document:   Document             = inject<Document>(DOCUMENT);
-  private readonly platformId: NonNullable<unknown> = inject<NonNullable<unknown>>(PLATFORM_ID);
+  private readonly afterViewInit$: WritableSignal<boolean> = signal<boolean>(false);
+  private readonly document:       Document                = inject<Document>(DOCUMENT);
+  private readonly ngZone:         NgZone                  = inject<NgZone>(NgZone);
+  private readonly platformId:     NonNullable<unknown>    = inject<NonNullable<unknown>>(PLATFORM_ID);
 
   public readonly stuck$: WritableSignal<boolean> = signal<boolean>(false);
 
-  protected readonly bodyOffsetHeight$:      Signal<number>  = toSignal<number>(
-    merge<[ number, number ]>(
-      this.document.defaultView ? fromEvent<Event>(
-        this.document.defaultView,
-        "resize",
-      ).pipe<number>(
-        map<Event, number>(
-          (): number => this.document.body.offsetHeight,
-        ),
-      ) : EMPTY,
-      toObservable<boolean>(
-        this.stuck$
-      ).pipe<number>(
-        map<boolean, number>(
-          (): number => this.document.body.offsetHeight,
+  protected readonly bodyHeight$:            Signal<number>  = toSignal<number>(
+    new Observable<number>(
+      (resizeEventObserver: Observer<number>): TeardownLogic => ((resizeObserver: ResizeObserver): TeardownLogic => {
+        resizeObserver
+          .observe(
+            this.document.body,
+          );
+
+        return resizeObserver.disconnect;
+      })(
+        new ResizeObserver(
+          (resizeObserverEntries: ResizeObserverEntry[]): void => this.ngZone.run<void>(
+            (): void => resizeEventObserver.next(
+              resizeObserverEntries[0].target.clientHeight,
+            ),
+          ),
         ),
       ),
     ).pipe<number, number>(
       startWith<number, [ number ]>(
-        this.document.body.offsetHeight,
+        this.document.body.clientHeight,
       ),
       distinctUntilChanged<number>(),
     ),
@@ -65,50 +65,146 @@ export class FooterComponent {
   protected readonly stuckOrUnsticking$:     Signal<boolean> = toSignal<boolean>(
     toObservable<boolean>(
       this.stuck$,
-    ).pipe<boolean, boolean>(
+    ).pipe<boolean, boolean, boolean>(
       delayWhen<boolean>(
         (stuck: boolean): Observable<number> => stuck ? timer(0) : timer(200),
       ),
-      startWith<boolean>(false),
+      startWith<boolean>(
+        this.stuck$(),
+      ),
+      distinctUntilChanged<boolean>(),
     ),
     {
       requireSync: true,
     },
   );
-  protected readonly containerOffsetBottom$: Signal<number>  = isPlatformBrowser(
+  protected readonly footerBottomProperty$:  Signal<number>  = isPlatformBrowser(
     this.platformId,
   ) ? toSignal<number>(
-    toObservable<number>(
-      this.bodyOffsetHeight$,
-    ).pipe<number, number, number, number>(
-      filter<number>(
-        (): boolean => this.footerHtmlElementRef?.nativeElement.parentElement ? getComputedStyle(
-          this.footerHtmlElementRef.nativeElement.parentElement,
-        ).getPropertyValue("position") === "static" : false,
+    this.document.defaultView ? merge<[ boolean, Event ]>(
+      toObservable<boolean>(
+        this.afterViewInit$,
       ),
-      map<number, number>(
-        (bodyOffsetHeight: number): number => bodyOffsetHeight - (this.footerHtmlElementRef?.nativeElement.parentElement?.offsetHeight || 0) - (this.footerHtmlElementRef?.nativeElement.parentElement?.offsetTop || 0) - (this.footerHtmlElementRef?.nativeElement.parentElement ? ((bottomPropertyValue: string): number => bottomPropertyValue.includes("px") ? Number(
+      fromEvent<Event>(
+        this.document.defaultView,
+        "resize",
+      ),
+    ).pipe<number, number, number>(
+      map<boolean | Event, number>(
+        (): number => this.footerHtmlElementRef?.nativeElement ? ((bottomPropertyValue: string): number => bottomPropertyValue.includes("px") ? Number(
           bottomPropertyValue.replace(
             "px",
             "",
           ),
         ) : 0)(
           getComputedStyle(
-            this.footerHtmlElementRef?.nativeElement.parentElement,
+            this.footerHtmlElementRef.nativeElement,
           ).getPropertyValue("bottom"),
-        ) : 0),
+        ) : 0,
       ),
       startWith<number, [ number ]>(
-        this.document.body.offsetHeight - (this.footerHtmlElementRef?.nativeElement.parentElement?.offsetHeight || 0) - (this.footerHtmlElementRef?.nativeElement.parentElement?.offsetTop || 0) - (this.footerHtmlElementRef?.nativeElement.parentElement ? ((bottomPropertyValue: string): number => bottomPropertyValue.includes("px") ? Number(
+        this.footerHtmlElementRef?.nativeElement ? ((bottomPropertyValue: string): number => bottomPropertyValue.includes("px") ? Number(
           bottomPropertyValue.replace(
             "px",
             "",
           ),
         ) : 0)(
           getComputedStyle(
-            this.footerHtmlElementRef?.nativeElement.parentElement,
+            this.footerHtmlElementRef.nativeElement,
           ).getPropertyValue("bottom"),
-        ) : 0),
+        ) : 0,
+      ),
+      distinctUntilChanged<number>(),
+    ) : EMPTY,
+    {
+      requireSync: true,
+    },
+  ) : signal<number>(0);
+  protected readonly footerHeight$:          Signal<number>  = isPlatformBrowser(
+    this.platformId,
+  ) ? toSignal<number>(
+    toObservable(
+      this.afterViewInit$,
+    ).pipe<number, number, number>(
+      switchMap<boolean, Observable<number>>(
+        (): Observable<number> => new Observable<number>(
+          (resizeEventObserver: Observer<number>): TeardownLogic => ((resizeObserver: ResizeObserver): TeardownLogic => {
+            this
+              .footerHtmlElementRef && resizeObserver
+              .observe(
+                this.footerHtmlElementRef.nativeElement,
+              );
+
+            return resizeObserver.disconnect;
+          })(
+            new ResizeObserver(
+              (resizeObserverEntries: ResizeObserverEntry[]): void => this.ngZone.run<void>(
+                (): void => resizeEventObserver.next(
+                  resizeObserverEntries[0].target.clientHeight,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      startWith<number, [ number ]>(
+        this.footerHtmlElementRef?.nativeElement.clientHeight || 0,
+      ),
+      distinctUntilChanged<number>(),
+    ),
+    {
+      requireSync: true,
+    },
+  ) : signal<number>(0);
+  protected readonly footerOffsetBottom$: Signal<number>  = isPlatformBrowser(
+    this.platformId,
+  ) ? toSignal<number>(
+    combineLatest<{ bodyHeight: Observable<number>, footerBottomProperty: Observable<number>, footerHeight: Observable<number> }>(
+      {
+        bodyHeight:           toObservable<number>(
+          this.bodyHeight$,
+        ),
+        footerBottomProperty: toObservable<number>(
+          this.footerBottomProperty$,
+        ),
+        footerHeight:         toObservable<number>(
+          this.footerHeight$,
+        ),
+      },
+    ).pipe<number, number, number>(
+      map<{ bodyHeight: number, footerBottomProperty: number, footerHeight: number }, number>(
+        (latest: { bodyHeight: number, footerBottomProperty: number, footerHeight: number }): number => latest.bodyHeight - (this.divHtmlElementRef?.nativeElement.offsetTop || 0) - latest.footerBottomProperty - latest.footerHeight,
+      ),
+      startWith<number, [ number ]>(
+        this.bodyHeight$() - (this.divHtmlElementRef?.nativeElement.offsetTop || 0) - this.footerBottomProperty$() - this.footerHeight$(),
+      ),
+      distinctUntilChanged<number>(),
+    ),
+    {
+      requireSync: true,
+    },
+  ) : signal<number>(0);
+  protected readonly backdropHeight$:        Signal<number>  = isPlatformBrowser(
+    this.platformId,
+  ) ? toSignal<number>(
+    combineLatest<{ footerHeight: Observable<number>, footerBottomProperty: Observable<number>, footerOffsetBottom: Observable<number> }>(
+      {
+        footerHeight:         toObservable<number>(
+          this.footerHeight$,
+        ),
+        footerBottomProperty: toObservable<number>(
+          this.footerBottomProperty$,
+        ),
+        footerOffsetBottom:   toObservable<number>(
+          this.footerOffsetBottom$,
+        ),
+      },
+    ).pipe<number, number, number>(
+      map<{ footerHeight: number, footerBottomProperty: number, footerOffsetBottom: number }, number>(
+        (latest: { footerHeight: number, footerBottomProperty: number, footerOffsetBottom: number }): number => latest.footerHeight + latest.footerOffsetBottom + latest.footerBottomProperty,
+      ),
+      startWith<number>(
+        this.footerHeight$() + this.footerOffsetBottom$() + this.footerBottomProperty$(),
       ),
       distinctUntilChanged<number>(),
     ),
@@ -119,42 +215,60 @@ export class FooterComponent {
   protected readonly unstickingTranslation$: Signal<number>  = isPlatformBrowser(
     this.platformId,
   ) ? toSignal<number>(
-    combineLatest<{ bodyOffsetHeight: Observable<number>, containerOffsetBottom: Observable<number>, scrollY: Observable<number>, stuckOrUnsticking: Observable<boolean> }>(
+    combineLatest<{ bodyHeight: Observable<number>, footerOffsetBottom: Observable<number>, scrollY: Observable<number>, viewportHeight: Observable<number> }>(
       {
-        bodyOffsetHeight:      toObservable<number>(
-          this.bodyOffsetHeight$,
+        bodyHeight:         toObservable<number>(
+          this.bodyHeight$,
         ),
-        containerOffsetBottom: toObservable<number>(
-          this.containerOffsetBottom$,
+        footerOffsetBottom: toObservable<number>(
+          this.footerOffsetBottom$,
         ),
-        scrollY:               fromEvent<Event>(
+        scrollY:            fromEvent<Event>(
           this.document,
           "scroll",
         ).pipe<number, number, number>(
           map<Event, number>(
-            (): number => this.document.defaultView?.scrollY || 0,
+            (): number => Math.max(
+              this.document.defaultView?.scrollY || 0,
+              0,
+            ),
           ),
           startWith<number, [ number ]>(
-            this.document.defaultView?.scrollY || 0,
+            Math.max(
+              this.document.defaultView?.scrollY || 0,
+              0,
+            ),
           ),
           distinctUntilChanged<number>(),
         ),
-        stuckOrUnsticking:     toObservable<boolean>(
-          this.stuckOrUnsticking$,
-        ),
+        viewportHeight:     this.document.defaultView ? fromEvent<Event>(
+          this.document.defaultView,
+          "resize",
+        ).pipe<number, number, number>(
+          map<Event, number>(
+            (): number => this.document.defaultView?.innerHeight || 0,
+          ),
+          startWith<number, [ number ]>(
+            this.document.defaultView?.innerHeight || 0,
+          ),
+          distinctUntilChanged<number>(),
+        ) : EMPTY,
       },
-    ).pipe<{ bodyOffsetHeight: number, containerOffsetBottom: number, scrollY: number, stuckOrUnsticking: true }, number, number, number>(
-      filter<{ bodyOffsetHeight: number, containerOffsetBottom: number, scrollY: number, stuckOrUnsticking: boolean }, { bodyOffsetHeight: number, containerOffsetBottom: number, scrollY: number, stuckOrUnsticking: true }>(
-        (latest: { bodyOffsetHeight: number, containerOffsetBottom: number, scrollY: number, stuckOrUnsticking: boolean }): latest is { bodyOffsetHeight: number, containerOffsetBottom: number, scrollY: number, stuckOrUnsticking: true } => latest.stuckOrUnsticking,
-      ),
-      map<{ bodyOffsetHeight: number, containerOffsetBottom: number, scrollY: number, stuckOrUnsticking: true }, number>(
-        (latest: { bodyOffsetHeight: number, containerOffsetBottom: number, scrollY: number, stuckOrUnsticking: true }): number => Math.max(
-          latest.bodyOffsetHeight - latest.scrollY - (this.document.defaultView?.innerHeight || 0) - latest.containerOffsetBottom,
+    ).pipe<number, number, number>(
+      map<{ bodyHeight: number, footerOffsetBottom: number, scrollY: number, viewportHeight: number }, number>(
+        (latest: { bodyHeight: number, footerOffsetBottom: number, scrollY: number, viewportHeight: number }): number => Math.max(
+          latest.bodyHeight - latest.scrollY - latest.viewportHeight - latest.footerOffsetBottom,
           0,
         ),
       ),
       startWith<number, [ number ]>(
-        this.document.body.offsetHeight - (this.document.defaultView?.scrollY || 0) - (this.document.defaultView?.innerHeight || 0),
+        Math.max(
+          this.bodyHeight$() - Math.max(
+            this.document.defaultView?.scrollY || 0,
+            0,
+          ) - (this.document.defaultView?.innerHeight || 0) - this.footerOffsetBottom$(),
+          0,
+        ),
       ),
       distinctUntilChanged<number>(),
     ),
@@ -179,15 +293,22 @@ export class FooterComponent {
   protected readonly raisedOrLowering$:      Signal<boolean> = toSignal<boolean>(
     toObservable<boolean>(
       this.raised$,
-    ).pipe<boolean, boolean>(
+    ).pipe<boolean, boolean, boolean>(
       delayWhen<boolean>(
         (stuck: boolean): Observable<number> => stuck ? timer(0) : timer(200),
       ),
       startWith<boolean>(false),
+      distinctUntilChanged<boolean>(),
     ),
     {
       requireSync: true,
     },
   );
+
+  public ngAfterViewInit(): void {
+    this
+      .afterViewInit$
+      .set(true);
+  }
 
 }
